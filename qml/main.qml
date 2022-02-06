3f59b1ec55100999e724029b7df9b848b2b9642e
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-02-05 20:56:25 -0600

    wip: add main qml example file

---
+++ qml/main.qml
@@ -0,0 +1,10 @@
+import QtQuick 2.0
+import QtQuick.Controls 2.0
+
+Window {
+    title: "Hello World"
+    width: 200
+    height: 200
+    visible: true
+    color: "white"
+}
---
+++ src/realbook/__main__.py
@@ -0,0 +1,21 @@
+import sys
+
+from PySide6.QtCore import QUrl
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtWidgets import QApplication
+
+
+def main():
+    app = QApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    engine.load(QUrl('qml/main.qml'))
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
